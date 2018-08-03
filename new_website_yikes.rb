1b638eaf5bb5c9ac0abd1b57c41ca6cf18164242
emilyambler <emilyambler4@gmail.com>
2018-08-03 15:39:17 +0000

    automatically backed up by learn

--- new_website_yikes.rb
+++ new_website_yikes.rb
@@ -1,6 +1,6 @@
 <h2>"Summer Programs"</h2>
         <br>
-        <h2>There are many summer programs and opportunities available for high school students just like you! Please note that many of the following offer need-based scholarships, so the tuition listed may differ based on situation.</h2>
+        <h2>"There are many summer programs and opportunities available for high school students just like you! Please note that many of the following offer need-based scholarships, so the tuition listed may differ based on situation."</h2>
         <br>
 	<a href=“#{link}”>#{program_title}</a>
         <h3>"This #{week} week camp takes place in #{location} and costs $#{cost}!"</h3>
